a394828581cad75f8c526df6143f54cc26237bcc
Joshua-mayor <joshuamayor56@gmail.com>
2023-10-06 18:19:09 -0700

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Program entry
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char m;
+
+	m = 'a';
+	while
+		(m <= 'z') {
+			putchar(m);
+			m++;
+		}
+	putchar('\n');
+	return (0);
+}
