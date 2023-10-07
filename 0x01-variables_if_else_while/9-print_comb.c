3f0e820fbe45a0b2566909690d4717e782474ac9
Joshua-mayor <joshuamayor56@gmail.com>
2023-10-06 19:53:05 -0700

    9-print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,4 +6,18 @@
  */
 
 int main(void)
+{
+	int z;
 
+	for (z = 0; z < 10; z++)
+	{
+		putchar(z + '0');
+		if (z < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
