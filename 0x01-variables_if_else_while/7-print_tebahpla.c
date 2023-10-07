ca162b718a12bba7daa2f70d235a5810e0c45d5f
Joshua-mayor <joshuamayor56@gmail.com>
2023-10-06 19:30:51 -0700

    7-print_tebahp

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Program entry
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char b;
+
+	b = 'z';
+	while
+		(b >= 'a') {
+			putchar(b);
+			b--;
+		}
+	putchar('\n');
+	return (0);
+}
