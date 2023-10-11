ff6390c6c2a23be4801b02249d9b1912860bbfd5
Joshua-mayor <joshuamayor56@gmail.com>
2023-10-10 17:39:13 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * -isalpha - Checks for the alphabetic chararcter character
+ *  @c: The character to br checked
+ *  Return: 1 for alphabetic character or 0 for anything else
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 97) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+		return (0);
+}
