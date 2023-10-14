1d0339bb4ed59209230b5a063acc66ff38f44d20
Joshua-mayor <joshuamayor56@gmail.com>
2023-10-14 07:12:03 -0700

    3-main.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_numbers - checks for digit (0 through 9)
+ * print_numbers - function that prints the numbers (from 0 to 9).
  *
- * Return: Always 0
+ * Return: Always 0.
  */
 
 void print_numbers(void)
@@ -14,5 +14,5 @@ void print_numbers(void)
 	{
 		_putchar(d);
 	}
-	_putchar("\n");
+		_putchar("\n");
 }
