a906e95aed1bdcefe4771b1d8e78c122b89f5748
Joshua-mayor <joshuamayor56@gmail.com>
2023-10-19 16:29:22 -0700

    1-strcat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,25 +8,25 @@
  * @n:input value
  * Return: dest
  */
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
-        int i;
-        int j;
+	int i;
+	int j;
 
-        i = 0;
-        while (dest[i] != '\0')
-        {
-                i++;
-        }
+	i = 0;
+	while (dest[i] != '\0')
+	{
+	i++;
+	}
 
-        j = 0;
-        while (j < n && src[j] != '\0')
-        {
-		dest[i] = src[j];
-                i++;
-                j++;
-        }
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+	dest[i] = src[j];
+	i++;
+	j++;
+	}
 
-        dest[i] = '\0';
-        return (dest);
+	dest[i] = '\0';
+	return (dest);
 }
