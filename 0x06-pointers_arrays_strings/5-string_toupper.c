f8fdbd9fd8ed03b298581eb1aa1cacd12799b67d
Joshua-mayor <joshuamayor56@gmail.com>
2023-10-22 14:25:45 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,16 +2,16 @@
 
 /**
  * string_toupper - converting any string to uppercase
- * @n: string param
+ * @n: pointer
  * Retrun: string
  */
 char *string_toupper(char *n)
 {
 	int i = 0;
 
-	while (n[i])
+	while (n[i] != '\0')
 	{
-		if (n[i] >= 97 && n[i] <= 122)
+		if (n[i] >= 'a' && n[i] <= 'z')
 			n[i] = n[i] - 32;
 		i++;
 	}
