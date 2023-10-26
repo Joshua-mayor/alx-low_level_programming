e4467c7429cffe33a9c196e0de56b3be8aace2ca
Joshua-mayor <joshuamayor56@gmail.com>
2023-10-25 17:15:32 -0700

    0-puts_recursion.c

--- 0x08-recursion/_putchar.c
+++ 0x08-recursion/_putchar.c
@@ -1,1 +1,12 @@
-#
+#include <unistd.h>
+
+/**
+ * _putchar - function writes the character c to stdoit
+ * @c: character to print
+ * return: on success 1
+ * on error, -1 is returned
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
